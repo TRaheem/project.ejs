db00760de8f8de3de6ef3ac1b0a48e6c75ab8c69
TRah <tbello58@gmail.com>
2020-07-17 12:24:37 -0600

    tweaks

--- public/js/index.js
+++ public/js/index.js
@@ -24,3 +24,9 @@
        gallery.innerHTML = imgTemplate;
    });
     
+      //hamburger menu.This line of code runs when the bar is clicked
+      $('.menu-toggle').click(function(){
+      $(".nav").toggleClass("portrait-nav");
+      $(this).toggleClass("is-active");
+      });
+  
